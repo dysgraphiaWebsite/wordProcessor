e5c3153a0c7e6348c9af62fa13dce2dec8734a3a
Andrew Carlson <acarl005@g.ucla.edu>
2015-10-17 15:31:32 -0700

    get editting functionality

--- client/controller.js
+++ client/controller.js
@@ -1,8 +1,26 @@
 $(document).ready(function() {
+
   $("#search").submit(function(e) {
     e.preventDefault();
-    $.get('/words/'+this.searchInput.value, function(data) {
-      console.log(data);
+    var value = this.searchInput.value;
+    var phrase = value.match(/\w+$/)[0];
+
+    $.get('/words/' + phrase, function(data) {
+      $('#suggest').empty();
+      data.forEach(function(word) {
+        $('#suggest').append(`<p class="suggestion"> ${word.word} </p>`);
+      });
     });
+
   });
+
+  $('#suggest').on('click', '.suggestion', function() {
+
+    var input = document.querySelector('#search input');
+    var value = input.value;
+    input.value = value.replace(/\w+$/, this.innerText + ' ');
+    $('#search input[type=text]').focus();
+    
+  });
+
 });
